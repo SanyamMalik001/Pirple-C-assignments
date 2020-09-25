7caeb82c0e906f8c2a2a2952d393829eee32c79c
SanyamMalik001 <50053596+SanyamMalik001@users.noreply.github.com>
2020-09-25 20:18:11 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,32 +11,27 @@ void showBoard(char arr[][3]){
 }
 int winnercheck(char arr[][3])
 {
-    for (int num = 0; num < 3; num++){
-             if (arr[num][0]== arr[num][1]&&arr[num][1]==arr[num][2]){
-                    if (num==0)
-                return 1;
-             else if (num == 1)
-                return 2;
-             else
-                return 3;
+             if (arr[0][0]== arr[0][1] && arr[0][1]==arr[0][2]){
+                return (1);}
+             else if (arr[1][0]== arr[1][1] && arr[1][1]==arr[1][2]){
+                return (2);}
+             else if(arr[2][0]== arr[2][1] && arr[2][1]==arr[2][2]){
+                return (3);}
+             else if(arr[0][0]==arr[1][0] && arr[2][0]==arr[2][0]){
+                return (4);}
+             else if (arr[0][1]==arr[1][1] && arr[2][1]==arr[2][1]){
+                return (5);}
+             else if(arr[0][2]==arr[1][2] && arr[2][2]==arr[2][2]){
+                return (6);}
+             else if(arr[0][0]==arr[1][1] && arr[1][1]==arr[2][2]){
+                return (7);
              }
-             else if(arr[0][num]==arr[1][num]&&arr[1][num]==arr[2][num]){
-                if (num==0)
-                return 4;
-             else if (num == 1)
-                return 5;
-             else
-                return 6;
-             }
-             else if(arr[0][0]==arr[1][1]&&arr[1][1]==arr[2][2]){
-                return 7;
-             }
-             else if (arr[0][2]==arr[1][1]&&arr[1][1]==arr[2][0]){
-                return 7;
+             else if (arr[0][2]==arr[1][1] && arr[1][1]==arr[2][0]){
+                return (7);
              }
              else{
                 return 0;
-             }}}
+             }}
 void declarewinner(void *ending ,int ch, char arr[][3]){
             if( ch==1&&arr[0][0]== 'X' ){
                 cout<<"Player X wins the top row. " <<endl;
@@ -91,7 +86,8 @@ int main()
         for (int x =1;x<10;x++){
     cout<< "\n Current board state \n"<<endl;
             showBoard(Arrchar);
-            declarewinner(ending,winnercheck(Arrchar), Arrchar);
+            int wc = winnercheck(Arrchar);
+            declarewinner(ending,wc, Arrchar);
        choose: if(x%2==1)
          cout<<"Player X, enter a number between 1 and 9:"<<endl;
          else
